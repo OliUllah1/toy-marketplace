547aee1e98476067a3b2e67a85e026deb91e26d2
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-19 11:40:16 +0600

    gallery pages done

---
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+
+        <div className=' bg-gray-300 rounded-lg px-5  pt-10'>
+            <div className='flex items-center pb-8'>
+            <h1 className=' text-4xl font-semibold '> <span className=' text-5xl text-pink-500'>T</span>oys <span className=' text-5xl text-pink-500'>G</span>allery </h1>
+            <p className='font-bold text-4xl text-pink-500'>__________</p>
+            </div>
+            
+            
+            <div className='grid grid-cols-4 pb-10 gap-5'>
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/wNh5KGL/front-view-cute-little-baby-boy-cupcake-teddybear.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/89fvnp2/n0jn-lo29-210929.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/9n3jq2g/dinosaur-with-spikes-back.jpg' alt="" />
+
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/wNh5KGL/front-view-cute-little-baby-boy-cupcake-teddybear.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/89fvnp2/n0jn-lo29-210929.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/9n3jq2g/dinosaur-with-spikes-back.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/wNh5KGL/front-view-cute-little-baby-boy-cupcake-teddybear.jpg' alt="" />
+            <img className=' shadow-xl h-full rounded-lg' src='https://i.ibb.co/89fvnp2/n0jn-lo29-210929.jpg' alt="" />
+            
+            
+            
+            
+        </div>
+        </div>
+    );
+};
+
+export default Gallery;
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import logo from '../../../assets/images/toy-logo.png';
 import { FaMap,FaEnvelope,FaPhoneAlt } from "react-icons/fa";
 const Footer = () => {
     return (
+      <div>
         <footer className="footer p-10 bg-base-200 text-base-content">
         <div>
         <div className='w-28'>
@@ -14,30 +15,37 @@ const Footer = () => {
     <p className='flex items-center font-semibold gap-3'><FaEnvelope className='text-xl text-pink-600'></FaEnvelope> Email : oliullah915@gmail.com</p>
     <p className='flex items-center font-semibold gap-3'> <FaPhoneAlt className='text-xl text-pink-600'></FaPhoneAlt>Phone : 0123587456</p>
         </div> 
-  <div className='lg:mt-10 space-y-2'>
+        <div className='lg:mt-10 space-y-2'>
     <span className="font-bold text-3xl">Quick Links</span> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Help Center</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Redeem Voucher</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Contact Us</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Policies & Rules</a>
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Check Offer List</a>
-  </div> 
-  <div className='lg:mt-10 space-y-2'>
+        </div> 
+        <div className='lg:mt-10 space-y-2'>
     <span className="font-bold text-3xl">Information</span> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Product Support</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Checkout</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">License Policy</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Affiliate</a>
     <a className=" text-gray-800 hover:text-sky-400 text-xl">About Us</a>
-  </div> 
-  <div className='lg:mt-10 space-y-2'>
+        </div> 
+        <div className='lg:mt-10 space-y-2'>
     <span className="font-bold text-3xl">Follow Us On</span> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Facebook</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Twitter</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Instagram</a> 
     <a className=" text-gray-800 hover:text-sky-400 text-xl">Youtube</a> 
-  </div>
-</footer>
+        </div>
+        
+        </footer>
+        <hr/>
+        <div className=' bg-slate-200 text-gray-500'>
+        <p className='py-3 text-center'>Copyright 2023 by Toys House All Right Reserved.</p>
+        </div>
+      </div>
+        
     );
 };
 
