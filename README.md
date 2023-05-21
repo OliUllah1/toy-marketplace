29992b25c6d607be6c74b698242586123317f35f
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-21 13:18:02 +0600

    readme file add

---
+++ README.md
@@ -0,0 +1,10 @@
+# Welcome To Toys House Repository
+## Web Site Details
+
+* Toys House is a e-commerce website.
+* You can see different types of animal toys on this website.
+* You can add any type of toys information through this website.
+* You can delete and update your added toy.
+* From this website you can buy high quality toys for your children.
+## 🔗 Links
+### Toys House Live Site ---- [ToysHouse.com](https://silver-mooncake-9b2713.netlify.app/ "title text!")
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,11 +6,11 @@ import { FaArrowRight } from "react-icons/fa";
 const Banner = () => {
     
     return (
-        <div className="carousel w-full h-[550px] rounded-xl">
+        <div className="carousel w-full lg:h-[550px] rounded-xl">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={banner} className="w-full object-cover" />
             <div className="absolute w-full h-full text-white gap-5 transform bg-gradient-to-r from-[#616464] to-[rgba(21, 21, 21,0)]">
-            <div className='px-20 pt-28'>
+            <div className='lg:px-20 lg:pt-28'>
             <h1 className='text-[#181d4e] text-6xl font-bold lg:w-4/5 mb-5 z-10'>Pick the best toy for your beloved child from <span className='text-6xl font-bold text-pink-500'>Toys</span> <span className=' text-yellow-400'>House</span></h1>
             <p className='w-2/3 font-semibold text-xl text-gray-800'>A kids toy shop is a store that specializes in selling toys and games designed specifically for children.</p>
             <button className='animate-bounce bg-sky-400 px-10 py-3 font-bold rounded-xl flex items-center gap-3 mt-10 hover:bg-pink-400'>SHOP NOW <FaArrowRight></FaArrowRight></button>
